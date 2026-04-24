ac8d3cc270ea8a643d4f252d92f2c5d55cad0866
dustysaturn <151056468+dustysaturn@users.noreply.github.com>
2026-04-24 16:10:15 +0100

    movement debugs

--- examples/controller.py
+++ examples/controller.py
@@ -12,6 +12,9 @@ class Controller():
     def fully_close_gripper(self, speed=20, force=255):
         self.gripper.move(self.gripper.get_max_position(), speed, force)
 
+    def set_gripper(self, position, speed=20, force=255):
+        self.gripper.move(position, speed, force)
+
     def go_home(self):
         self.robot.go_home()
         
@@ -22,7 +25,16 @@ class Controller():
         pass
         
     def vial_gripped(self) -> bool:
-        if self.gripper.is_closed():
+        print(f"Is Closed: {self.gripper.is_closed()}")
+        print(f"Is Open: {self.gripper.is_open()}")
+        print(f"Get Closed {self.gripper.get_closed_position()}")
+        print(f"Get Current Pos{self.gripper.get_current_position()}")
+        print(f"Get Max Pos{self.gripper.get_max_position()}")
+        print(f"Get Min Pos{self.gripper.get_min_position()}")
+        print(f"{self.gripper.GripperStatus}")
+        epsilon = 50
+
+        if self.gripper.get_current_position() >= (self.gripper.get_closed_position() - epsilon):
             return False
         else:
             return True
